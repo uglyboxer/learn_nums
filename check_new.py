e50cc5711f39a237b2de232196f43f0eb8a7b632
Cole Howard <cole@subtlegears.com>
2015-09-20 22:02:40 -0700

    Clean up output, add percentage

--- check_new.py
+++ check_new.py
@@ -1,12 +1,24 @@
 from dot_product import dot_product
 
 def check_new(vector, weights):
-	""" Takes in a vector (representing a number) and weights
-	Returns a guess as so it's value
+	""" Takes in a vector (representing a picture of a number) and weights
+	Returns a guess as to it's value
 	"""
 	threshold = .5
 	computer_guesses = []
+	dot_product_list = []
 	for x in range(10):
-		if dot_product(vector, weights[x]) > threshold:
+		dot_product_list.append(dot_product(vector, weights[x]))
+		if dot_product_list[x] > threshold:
 			computer_guesses.append(x)
-	return computer_guesses
+	if dot_product_list:
+		return dot_product_list.index(max(dot_product_list))
+	else:
+		return None
+
+
+
+
+
+
+
--- learn_nums.py
+++ learn_nums.py
@@ -13,5 +13,13 @@ digits = datasets.load_digits()
 answers = digits.target
 sliced_digits = digits.data[:1000]
 weight_set_of = [learn_loop(sliced_digits, answers, x) for x in range(10)]		#gets back weights
-temp = 1004
-print("Computer's guess: ", check_new(digits.data[temp], weight_set_of), " Actual #: ", digits.target[temp])
+successes = 0
+for temp in range(1001, len(digits.data)):
+	guess = check_new(digits.data[temp], weight_set_of)
+	actual = digits.target[temp]
+	print("Computer's guess: {}  Actual #: {}".format(guess, actual))
+	if guess == actual:
+		successes += 1
+print("Computer was right {} times out of {}".format(successes, len(digits.data)-1000))
+print("For a correct percentage of {}%".format(successes/(len(digits.data)-1000)))
+
